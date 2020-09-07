67cb7f7d08c08b9814ea11b7771be000dfc08d1a
cheers32 <cheers32@outlook.com>
2020-09-07 11:33:12 -0400

    Q003

---
+++ Algos/Q003.py
@@ -0,0 +1,22 @@
+def lengthOfLongestSubstring(s):
+    map = {}  # char -> idx
+    idx = 0
+    start = 0
+    maxLen = 0
+    for c in s:
+        if c not in map:
+            map[c] = idx
+        else:
+            start = max(start, map[c]+1)  # this is important step, abba example, the start cannot be less than current start
+            map[c] = idx
+        len = idx-start+1
+        maxLen = max(len, maxLen)
+        idx += 1
+    return maxLen
+
+
+#print(lengthOfLongestSubstring("abcbef"))
+#print(lengthOfLongestSubstring("abcabcbb"))
+print(lengthOfLongestSubstring("abba"))
+
+# another way is 26n, was my original, use a map to count, when c in set, remove from start, until no dup then to expand, not very efficient
